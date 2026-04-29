8a331b1834c6e283cff2052cb181f008a1e610c8
Tenerife-Q <2892015852@qq.com>
2026-04-29 20:38:08 +0800

    refactor: extract calculate_hash method from Block::new

--- src/main.rs
+++ src/main.rs
@@ -37,11 +37,12 @@ impl Blockchain {
 
 
 impl Block {
-    fn new(index: u64, data: String, pre_hash: String) -> Block {
+    fn new(index: u64, data: String, pre_hash: String) -> Self {
         let timestamp = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .unwrap()
             .as_secs();
+        /* 
         let mock_hash = format!("{}{}{}{}", index, data, pre_hash, timestamp);
         let mut hasher = Sha256::new();
         hasher.update(mock_hash.as_bytes());
@@ -49,14 +50,35 @@ impl Block {
         let hash: String = hash_result
             .iter()
             .map(|byte| format!("{:02x}", byte))
+            // format!("{:02x}", byte) 将每个字节格式化为两位十六进制字符串，
+            // 确保单个字节（0-255）总是占用两位（例如，0x0a会被格式化为"0a"而不是"a"
+            // x表示十六进制，02表示至少两位，不足两位时在前面补0。
+            // 大括号里，冒号左边是变量的位置（留空就是按先后顺序接纳变量）
             .collect();
-        Block {
+        */
+        let mut block = Block {
             index,
             timestamp,
             data,
             pre_hash,
-            hash,
-        } 
+            hash: String::new(), // 先占位，后面计算hash
+        };
+        block.hash = block.calculate_hash();
+        block
+
+    }
+
+    fn calculate_hash(&self) -> String {
+        let mock_hash = format!(
+            "{}{}{}{}",self.index, self.data, self.pre_hash, self.timestamp
+        );
+        let mut hasher = Sha256::new();
+        hasher.update(mock_hash.as_bytes());
+        let hash_result = hasher.finalize();
+        hash_result.
+            iter()
+            .map(|byte| format!("{:02x}", byte))
+            .collect()
     }
 }
 
@@ -65,5 +87,9 @@ fn main() {
     blockchain.add_block("Second_Block".to_string());
     blockchain.add_block("Third_Block".to_string());
     println!("{:#?}", blockchain);
+// // {:?} 是一行流打印，紧凑但可能很长
+// format!("{:?}", blockchain); 
 
+// // {:#?} 中的 # 意思是 "Pretty-print"（美化打印），它会自动帮你换行、缩进，看起来像 JSON。
+// format!("{:#?}", blockchain); 
 }
